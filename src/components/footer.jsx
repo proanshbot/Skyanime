2180d021eae09fb7df02f360d3266b234bf1e2c0
GodxD69 <142318064+GodxD69@users.noreply.github.com>
2024-08-25 02:27:29 +0800

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -70,7 +70,7 @@ const SocialIconsWrapper = styled.div`
 `;
 
 const FooterLogoImage = styled.img`
-  content: url('src/images/icon.png');
+  content: url('../images/icon.png');
   max-width: 8rem;
   height: auto;
 `;
