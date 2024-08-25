49a5d153060170edbfb38729c4b0301571601e0c
GodxD69 <142318064+GodxD69@users.noreply.github.com>
2024-08-26 01:15:37 +0800

    Update google-gemini-effect.tsx

--- src/components/ui/google-gemini-effect.tsx
+++ src/components/ui/google-gemini-effect.tsx
@@ -22,7 +22,7 @@ export const GoogleGeminiEffect = ({
   return (
     <div className={cn("sticky top-40", className)}>
       <p className="text-[45px] md:text-7xl font-normal pt-16 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-100 to-neutral-300">
-        {`Anveshna.`}
+        {`FIREANIME`}
       </p>
       <p className="text-[20px] md:text-xl font-normal text-center text-neutral-400 mt-4 max-w-lg mx-auto">
         {description ||
